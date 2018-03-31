d846a405f4ed765730d274e4d4b4b979748fe571
Dave Winer <dave.winer@gmail.com>
2018-03-31 10:53:47 -0400

    v0.4.0

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # feedRead
+
 Read a feed and call back with a JavaScript structure.
+
+### What is this?
+
+It's a Node package that contains everything you need to read a feed. 
+
+It builds on the feedParser package. It's simpler to call, no need to master streams or iconv. 
+
+It handles encoding using the iconv package. 
+
+You can also parse text, so you can deal with files that contain feed data. 
+
+Because we use feedParser, we handle all the formats and variability that it handles. 
+
+It's an entry-level parser that will be useful to people who haven't mastered all of JavaScript. 
+
+### Why?
+
+Sometimes you need to quickly read a feed and do something with it, and don't have time for a major project. This package is for those times. 
+
---
+++ feedread.js
@@ -0,0 +1,132 @@
+var myProductName = "feedread"; myVersion = "0.4.0";
+
+/*  The MIT License (MIT)
+	Copyright (c) 2014-2017 Dave Winer
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+	*/
+
+exports.parseString = parseFeedString;
+exports.parseUrl = parseFeedUrl;
+exports.getCharset = getCharset;
+
+const utils = require ("daveutils");
+const feedParser = require ("feedparser");
+const request = require ("request");
+const stream = require ("stream");
+const Iconv = require ("iconv").Iconv;
+
+const metaNames = { 
+	title: true,
+	link: true,
+	description: true,
+	pubDate: true,
+	language: true,
+	copyright: true,
+	generator: true,
+	cloud: true,
+	image: true,
+	categories: true
+	};
+
+function getCharset (httpResponse) {
+	var contentType = httpResponse.headers ["content-type"];
+	if (contentType !== undefined) {
+		var encoding = utils.trimWhitespace (utils.stringNthField (contentType, ";", 2));
+		if (encoding.length > 0) {
+			var charset = utils.trimWhitespace (utils.stringNthField (encoding, "=", 2));
+			return (charset);
+			}
+		}
+	return (undefined); //no charset specified
+	}
+function parseFeedString (theString, charset, callback) {
+	var feedparser = new feedParser ();
+	var theFeed = {
+		head: new Object (),
+		items: new Array ()
+		};
+	
+	if (charset !== undefined) {
+		var iconv = new Iconv (charset, "UTF-8");
+		theString = iconv.convert (theString).toString ();
+		}
+	
+	var theStream = new stream.Readable;
+	theStream.push (theString);
+	theStream.push (null);
+	
+	feedparser.on ("readable", function () {
+		try {
+			var item = this.read ();
+			if (item !== null) {
+				theFeed.items.push (item);
+				for (var x in item.meta) {
+					if (metaNames [x] !== undefined) {
+						theFeed.head [x] = item.meta [x];
+						}
+					}
+				}
+			}
+		catch (err) {
+			console.log ("parseFeedString: err.message == " + err.message);
+			}
+		});
+	feedparser.on ("error", function (err) {
+		console.log ("parseFeedString: err.message == " + err.message);
+		callback (err);
+		});
+	feedparser.on ("end", function () {
+		callback (undefined, theFeed);
+		});
+	
+	theStream.pipe (feedparser);
+	}
+function parseFeedUrl (feedUrl, timeOutSecs, callback) {
+	var theRequest = {
+		url: feedUrl, 
+		encoding: null,
+		jar: true,
+		gzip: true,
+		maxRedirects: 5,
+		headers: {
+			"User-Agent": myProductName + " v" + myVersion
+			}
+		};
+	if (timeOutSecs !== undefined) {
+		theRequest.timeout = timeOutSecs * 1000;
+		}
+	request (theRequest, function (err, response, theString) {
+		if (err) {
+			callback (err);
+			}
+		else {
+			if (response.statusCode != 200) {
+				var theErrorResponse = {
+					message: "Error reading the feed, response.statusCode == " + response.statusCode + ".",
+					statusCode: response.statusCode
+					};
+				callback (theErrorResponse);
+				}
+			else {
+				parseFeedString (theString, getCharset (response), callback);
+				}
+			}
+		});
+	}
